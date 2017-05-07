c6c3accaef7376d67ba319720526676780612a6e
AoEiuV020 <aoeiuv020@gmail.com>
2017-05-07 12:52:54 +0800

    Test generics get set

--- app/src/test/kotlin/aoeiuv020/GenericsTest.kt
+++ app/src/test/kotlin/aoeiuv020/GenericsTest.kt
@@ -16,13 +16,25 @@ class GenericsTest {
         val cb1: CB<Number> = CB()
         //val cb2: CB<Any> = cb1 //e: Type mismatch
         val cb2: CB<Int> = cb1
-        val cc1: CC<Number> = CC()
+        var any: Any
+        cb1.set(1)
+        cb2.set(1)
+        val cc1: CC<Number> = CC(1.0)
         val cc2: CC<Any> = cc1
         //val cc2: CC<Int> = cc1 //e: Type mismatch
+        any = cc1.get()
+        assertEquals(1.0, any)
+        assertEquals(Double::class, any::class)
+        any = cc2.get()
+        assertEquals(1.0, any)
     }
 
     class CA<T>
-    class CB<in T>
-    class CC<out T>
+    class CB<in T> {
+        fun set(t: T) {}
+    }
+    class CC<out T>(val t: T) {
+        fun get(): T = t
+    }
 }
 
