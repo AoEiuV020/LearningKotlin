9c269cde8e08b14402ad7168467b90e3ffb8045d
AoEiuV020 <aoeiuv020@gmail.com>
2017-06-29 18:04:15 +0800

    Recreate project
    Intellij以普通项目打开，不用gradle,
    顺便添加okhttp模块，

---
+++ okhttp/src/test/kotlin/aoeiuv020/okhttp/ws/EchoTest.kt
@@ -0,0 +1,16 @@
+package aoeiuv020.okhttp.ws
+
+import okhttp3.OkHttpClient
+import org.junit.Test
+import kotlin.test.assertTrue
+
+/**
+ * Created by AoEiuV020 on 2017.06.29-17:36:08.
+ */
+class EchoTest {
+    @Test
+    fun echo() {
+        val client = OkHttpClient()
+        assertTrue { true }
+    }
+}
